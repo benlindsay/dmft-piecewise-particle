12eefd5fb2d5771dcfa17c1c67e77281111e7d79
Ben Lindsay <benjlindsay@gmail.com>
2018-08-25 22:41:09 -0400

    added bridges makefile

---
+++ makefiles/makefile-bridges.mk
@@ -0,0 +1,28 @@
+CC = icpc
+#CFLAGS = -pg -fopenmp
+CFLAGS = -O3 -fopenmp -Wall -I$(FFTW3_INCLUDE)
+LIBS = -lm -lfftw3_omp -lfftw3 -lpthread -L$(FFTW3_LIB)
+TARGET      = $(shell echo dmft-$$(git describe --tags))
+
+
+#############################################################################
+# nothing should be changed below here
+
+SRCS = time.cpp stress.cpp main.cpp matrix.cpp array_utils.cpp die.cpp \
+       random.cpp grid_utils.cpp torque.cpp quanterions.cpp fftw_wrappers.cpp \
+       initialize.cpp config_utils.cpp io_utils.cpp update_euler.cpp \
+       update_positions.cpp forces.cpp integ_utils.cpp read_input.cpp \
+       bonded.cpp calc_unb.cpp
+
+OBJS = ${SRCS:.cpp=.o}
+
+.cpp.o:
+	${CC} ${CFLAGS} ${DFLAGS} -c  $<
+
+$(TARGET):  ${OBJS}
+	$(CC) ${CFLAGS} ${DFLAGS} -o $@ ${OBJS} $(LIBS)
+
+clean:
+	rm -f *.o
+	rm -f $(TARGET)
+	rm -f *~
